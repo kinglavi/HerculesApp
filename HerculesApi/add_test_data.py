d0ae3c789ca0ea624efe657f7266d08da93ab99d
klavior <klavior@gmail.com>
2016-06-28 01:38:08 +0300

    finished serializers without checks

---
+++ HerculesApi/add_test_data.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import *
+from HerculesApi.models import *
+from datetime import timedelta
+import datetime
+
+u = User(username='kinglavi')
+u.save()
+g = Group(name='kinglavi_group')
+g.save()
+u.groups = []
+u.groups.add(g)
+c = Company(name='laviCompany', description='This is lavi company.', managers=g)
+c.save()
+s = Store(company=c, address='Lavi Street, Kinglavi country', phone_number="0501234352")
+s.save()
+p = Product(name='lavi_product', description='This is Lavi product.', price=50)
+p.save()
+camp = Campaign(store=s, product=p, goal=10, description="The best campaign ever.", gift_discount=100,
+                end_date=datetime.datetime.now() + timedelta(days=50))
+camp.save()
+stick = Sticker(token='lavigever', is_used=False, product=p)
+stick.save()
+cust = Customer(username='kinglavi_customer')
+cust.save()
+card = Card(campaign=camp, owner=cust)
+card.save()
