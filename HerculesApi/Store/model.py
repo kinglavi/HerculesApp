3e9e19c0636f88e99dbe37339a1f5beb1f0d156b
kinglavi <klavior@gmail.com>
2016-10-19 22:42:12 +0300

    start night 19.10

--- HerculesApi/Store/model.py
+++ HerculesApi/Store/model.py
@@ -2,9 +2,12 @@ from HerculesApi.Company.model import Company
 from django.contrib.auth.models import Group
 from django.db import models
 
+from HerculesApi.Validators.validators import GLOBAL_ALPHANUMERIC_NAME_VALIDATOR
+
 
 class Store(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,
+                            validators=[GLOBAL_ALPHANUMERIC_NAME_VALIDATOR])
     company = models.ForeignKey(Company)
     address = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
